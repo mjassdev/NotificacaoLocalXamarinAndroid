af5d28ef69fbd20dc76ebe10e0309a4fa937f0ff
System Administrator <root@MacBook-Pro-de-usuario.local>
2018-07-05 15:16:43 -0300

    Atualizando

--- TesteNotificationDroid/MainActivity.cs
+++ TesteNotificationDroid/MainActivity.cs
@@ -4,6 +4,8 @@ using Android.OS;
 using Android.Content;
 using Android.Support.V4.App;
 using System.Threading;
+using System;
+using TesteNotificationDroid.ModelsHorarios;
 
 namespace TesteNotificationDroid
 {
@@ -13,9 +15,6 @@ namespace TesteNotificationDroid
 
         private static readonly int ButtonClickNotification = 9999;
 
-
-
-
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -24,23 +23,38 @@ namespace TesteNotificationDroid
 
         }
 
-
         protected override void OnStop()
         {
+            bool a = true;
+            while (a){
+                VerificaNotificacao();
+                Thread.Sleep(3600000);
+            }
 
-            Thread.CurrentThread.Name = "Notificacao";
+            base.OnStop();
+        }
+
+
+
+        public void VerificaNotificacao()
+        {
+            //Thread.CurrentThread.Name = "verifica";
             Thread t1 = new Thread(new ThreadStart(run));
             t1.Start();
 
             void run()
             {
-                bool a = true;
+                
+                TimeSpan comeco = new TimeSpan(14, 40, 00); //Define inicio do disparo da thread 00:00:00
+                TimeSpan fim = new TimeSpan(15, 32, 00); //Define o fim do intervalo da thread
+                TimeSpan agora = DateTime.Now.TimeOfDay; //Obtem a horario atual
 
-                while (a)
+                if ((agora >= comeco) && (agora < fim)) // condicao para exibir a notificacao dentro do intervalo definido
                 {
-                    Thread.Sleep(10000);
+                    
+
                     Bundle valueSend = new Bundle();
-                    valueSend.PutString("sendContent", "Teste Sucesso");
+                    valueSend.PutString("sendContent", "Teste Sucesso"); //Destino do tapped da notificação
 
                     Intent newIntent = new Intent(this, typeof(Second_Activity));
                     newIntent.PutExtras(valueSend);
@@ -54,20 +68,21 @@ namespace TesteNotificationDroid
                     NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
                         .SetAutoCancel(true)
                         .SetContentIntent(resultPendingIntent)
-                        .SetContentTitle("Notificação")
-                        .SetSmallIcon(Resource.Drawable.icon)
-                        .SetContentText("Clique aqui");
+                        .SetContentTitle("SESC TREINO") // Define titulo da notificação;
+                        .SetSmallIcon(Resource.Drawable.icon) //Define ícone da notificacao;
+                        .SetContentText("Você tem um novo aviso. Clique aqui"); // Texto da notificação
 
                     NotificationManager notificationManager = (NotificationManager)GetSystemService(Context.NotificationService);
                     notificationManager.Notify(ButtonClickNotification, builder.Build());
 
                 }
+
+
             }
+         }
+
+      }
 
 
-            base.OnStop();
-
-        }
-    }
 }
 
---
+++ TesteNotificationDroid/ModelsHorarios/Agendamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+
+namespace TesteNotificationDroid.ModelsHorarios
+{
+    public class Agendamento
+    {
+
+        TimeSpan comeco = new TimeSpan(01, 37, 00);
+        TimeSpan fim = new TimeSpan(01, 37, 01);
+        TimeSpan agora = DateTime.Now.TimeOfDay;
+
+    }
+}
